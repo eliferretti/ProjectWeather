d92b74e3a360c4bb56529428ef6bd9149d0dc29b
eliferretti <eliferretti14@gmail.com>
2023-10-25 11:39:33 -0300

    api

---
+++ src/ProjectWeather.Application/Helpers/MapProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ProjectWeather.Application.Dto;
+using ProjectWeather.Domain.Entities;
+
+namespace ProjectWeather.Application.Helpers
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile() 
+        {
+            CreateMap<Weather, AddWeatherDto>();
+        }
+    }
+}
