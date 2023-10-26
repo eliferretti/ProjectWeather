0e0bb4bebe7b987ca133ed892a3099870e33b4ce
eliferretti <eliferretti14@gmail.com>
2023-10-26 10:53:52 -0300

    map

--- src/ProjectWeather.Application/Helpers/MapProfile.cs
+++ src/ProjectWeather.Application/Helpers/MapProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using ProjectWeather.Application.Dto;
 using ProjectWeather.Domain.Entities;
+using ProjectWeather.Domain.ValueObjects;
 
 namespace ProjectWeather.Application.Helpers
 {
@@ -8,7 +9,10 @@ namespace ProjectWeather.Application.Helpers
     {
         public MapProfile() 
         {
-            CreateMap<Weather, AddWeatherDto>();
+            CreateMap<Weather, AddWeatherDto>().ReverseMap();
+            CreateMap<Location, AddLocationDto>().ReverseMap();
+            CreateMap<Condition, AddConditionDto>().ReverseMap();
+            CreateMap<Current, AddCurrentDto>().ReverseMap();
         }
     }
 }
